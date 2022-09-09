f5fb8789da25bc9c7e7b4e250b484ddb953034ce
Alexander Eismont <alexander0390@hotmail.com>
2022-09-09 18:53:45 +0200

    basic golang cli

---
+++ go/go-cli/gocli.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
+var app = cli.NewApp()
+
+func main() {
+	info()
+	cliConfig()
+	commands()
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func info() {
+	app.Name = "Simple GOLANG Cli"
+	app.Usage = "Commands"
+	app.Version = "1.0.0"
+}
+
+func cliConfig() {
+	app.EnableBashCompletion = true
+}
+
+func commands() {
+	app.Commands = []*cli.Command{
+		{
+			Name:    "greeting",
+			Aliases: []string{"g"},
+			Usage:   "prints welcome message",
+			Action: func(cCtx *cli.Context) error {
+				if cCtx.Bool("username") {
+					fmt.Println("Hello user: ", cCtx.Args().First())
+				}
+				fmt.Println("Welcome to this cli ")
+				return nil
+			},
+			Flags: []cli.Flag{
+				&cli.BoolFlag{
+					Name:    "username",
+					Aliases: []string{"u"},
+					Usage:   "prints a username with a hello message",
+				},
+			},
+		},
+	}
+}
